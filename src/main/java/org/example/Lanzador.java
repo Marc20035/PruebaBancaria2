35f8d0a05cc32e947917938d49a30b9b3c34ca7b
Marc <mlopesab@myuax.com>
2023-10-05 10:34:56 +0200

    CommitMain

--- src/main/java/org/example/Lanzador.java
+++ src/main/java/org/example/Lanzador.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Lanzador {
-    public static void main(String[] args) {
+    public static void mostrarMain() {
         Cuenta cuenta = new Cuenta(10000);
 
         // Crear hilos para cada tipo de transacción
